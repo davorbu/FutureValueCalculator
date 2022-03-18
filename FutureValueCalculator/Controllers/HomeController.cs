3eb54e076fb89a7949b773a0a861b994a1f3d0d0
Davor Burmudžija <davorburmudzija@gmail.com>
2022-03-18 20:50:50 +0100

    Add project files.

---
+++ FutureValueCalculator/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using FutureValueCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FutureValueCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FutureValue = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel fv)
+        {
+            if (ModelState.IsValid)
+            {
+              ViewBag.FutureValue = fv.Calculate().ToString("c2");
+            }
+            else
+            {
+                ViewBag.FutureValue = "";
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
